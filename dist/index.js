595f7cd45723cdf5b354427983ffc336786cb367
raja <arajavignesh@gmail.com>
2022-12-13 11:42:00 +0000

    video02

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let id = 10;
+let company = 'Bissing';
+let employee = false;
+let hobbies = 1;
+hobbies = false;
+hobbies = 'brick';
+let friends = ['A', 'B', 'c'];
+let grades = [1, 2, 3];
+let student = ['Test', 100, false];
+let students = [['Test', 100, false], ['Test 1', 10, false]];
+
+let arr = [1,2];
+let [a,b=0] = arr;
+
+console.log(a,b);
+console.log(b);
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let id: number = 10
+
+let company:string = 'Bissing'
+
+let employee:boolean = false
+
+let hobbies:any = 1
+
+hobbies = false
+
+hobbies = 'brick'
+
+let friends: string[] = ['A','B','c']
+
+let grades:number[] = [1,2,3]
+
+let student:[string,number,boolean] = ['Test',100,false]
+
